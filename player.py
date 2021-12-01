96bffc77191e1be6e56857d53119998d2a2a8640
Joshua Medvinsky <belt123456@gmail.com>
2021-11-30 16:02:18 -0800

    Methods for player.py

--- player.py
+++ player.py
@@ -12,24 +12,44 @@ import numpy as np
 
 class player(object):
         """
-            This is the player class
-        
-        """
-        
+            This is the player class 
+            """
         def __init__(self, player_type, memorization_value, reaction_value, placing_value, 
                      miss_slap_value, deck):
             self.player_type = player_type
             self.memorization_value = memorization_value
             self.reaction_value = reaction_value
             self.placing_value = placing_value
+            #The higher the worse for miss slap value
             self.miss_slap_value = miss_slap_value
             self.deck = deck
             self.memorized_deck = []
             self.miss_slaps = 0
             self.slaps = 0
             self.wins = 0
+            #pre-slap
             
+        def get_reaction_time(self):
             
+            #current rudementary logic (to be changed later)
+            reaction_time = (float)(np.random.randint(5,10)/self.reaction_value)
             
+            return reaction_time
+        
+        def get_placing_time(self):
             
+            #current rudementary logic (to be changed later)
+            placing_time = (float) (np.randint(4,8)/self.placing_value)
+            
+            return placing_time
+            
+        def miss_slap_occured(self):
+            #I like this logic, 10% is reasonable
+            miss_slap = False
+            
+            if np.random.randint(0,100) < self.miss_slap_value:
+                miss_slap = True
+                self.slaps+=1
+                
+            return miss_slap
         
